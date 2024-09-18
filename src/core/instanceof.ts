08d536dd5f6841c7604a5a89a8c4d3c9832f90cd
RGCHN <dacong9610@163.com>
2024-09-18 11:43:40 +0800

    style: 优化代码格式

--- src/core/instanceof.ts
+++ src/core/instanceof.ts
@@ -1,53 +1,52 @@
-// @ts-nocheck
+// @ts-expect-error
 const renderTarget = typeof my !== 'undefined' ? my.renderTarget : undefined;
 const inMiniPlugin = renderTarget === 'web' && typeof getApp !== 'function';
+const isGlobalConstructor = function (k: any): boolean {
+  if (typeof k === 'function' && typeof k.name === 'string') {
+    let gk;
 
-export function isInstanceOf (a, b) {
+    switch (k.name) {
+      case 'Object': gk = Object;
+
+        break;
+      case 'Array': gk = Array;
+
+        break;
+      case 'String': gk = String;
+
+        break;
+      case 'Number': gk = Number;
+
+        break;
+      case 'Date': gk = Date;
+
+        break;
+      case 'Function': gk = Function;
+
+        break;
+      case 'Boolean': gk = Boolean;
+
+        break;
+      case 'ArrayBuffer': gk = typeof ArrayBuffer !== 'undefined' ? ArrayBuffer : undefined;
+
+        break;
+      case 'Promise': gk = typeof Promise !== 'undefined' ? Promise : undefined;
+
+        break;
+    }
+
+    return gk === k;
+  }
+
+  return false;
+};
+
+export function isInstanceOf (a: any, b: any) {
   const result = a instanceof b;
 
   if (result) {
     return result;
   } else if (inMiniPlugin) {
-    const isGlobalConstructor = function (k) {
-      if (typeof k === 'function' && typeof k.name === 'string') {
-        let gk;
-
-        switch (k.name) {
-          case 'Object': gk = Object;
-
-            break;
-          case 'Array': gk = Array;
-
-            break;
-          case 'String': gk = String;
-
-            break;
-          case 'Number': gk = Number;
-
-            break;
-          case 'Date': gk = Date;
-
-            break;
-          case 'Function': gk = Function;
-
-            break;
-          case 'Boolean': gk = Boolean;
-
-            break;
-          case 'ArrayBuffer': gk = typeof ArrayBuffer !== 'undefined' ? ArrayBuffer : undefined;
-
-            break;
-          case 'Promise': gk = typeof Promise !== 'undefined' ? Promise : undefined;
-
-            break;
-        }
-
-        return gk === k;
-      }
-
-      return false;
-    };
-
     if (isGlobalConstructor(b)) {
       const aType = Object.prototype.toString.call(a);
 
